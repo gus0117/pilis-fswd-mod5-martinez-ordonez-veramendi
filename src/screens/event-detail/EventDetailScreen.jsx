8d22c776ec0add22ee96652a42ee4caa75adae10
Brenda Veramendi <brendaveramendi@gmail.com>
2023-04-19 20:45:24 -0300

    Detail

--- src/screens/event-detail/EventDetailScreen.jsx
+++ src/screens/event-detail/EventDetailScreen.jsx
@@ -1,13 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { View, ScrollView, Image, Text } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
+import { styles } from './EventDetailScreen.styles'
+import { Ionicons } from '@expo/vector-icons'
+import { COLORS } from '../../utils/theme'
 
-export const EventDetailScreen = () => {
+export const EventDetailScreen = ({ route }) => {
+  const { item } = route.params
   return (
-    <View>
-      <Text>EventDetailScreen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ScrollView horizontal pagingEnabled style={styles.imageContainer}>
+          {item.images.map((image, idx) => (
+            <Image
+              key={idx}
+              source={{ uri: `https://drive.google.com/uc?id=${image}` }}
+              style={styles.image}
+              resizeMode='cover'
+            />
+          ))}
+        </ScrollView>
+      </View>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.location}>{item.location}</Text>
+        <Text style={styles.date}>{item.date}</Text>
+        <View style={styles.ratingContainer}>
+          <Ionicons name='star' size={20} color={COLORS.primary} />
+          <Text style={styles.rating}>{item.rating}</Text>
+        </View>
+        
+        <Text style={styles.description}>{item.description}</Text>
+      </View>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: item.locationCoordinates.latitude,
+          longitude: item.locationCoordinates.longitude,
+          latitudeDelta: 0.002,
+          longitudeDelta: 0.002
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: item.locationCoordinates.latitude,
+            longitude: item.locationCoordinates.longitude
+          }}
+          title={item.title}
+        />
+      </MapView>
+    </ScrollView>
   )
 }
-
-
-const styles = StyleSheet.create({})
