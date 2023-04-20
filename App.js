078adad922be4fd3b084d083e34fba1e72c0f6d5
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2023-04-20 00:16:25 -0300

    Agregada logica de UserContext

--- App.js
+++ App.js
@@ -2,17 +2,20 @@ import { StatusBar } from 'expo-status-bar'
 import { MainStackScreen } from './src/screens/event-list/MainStackScreen'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { UserProvider } from './src/contexts/UserContext'
 
 const LocationListStack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <>
-      <NavigationContainer>
-            <LocationListStack.Navigator screenOptions={{ headerShown: false }}>
-              <LocationListStack.Screen name='Main' component={MainStackScreen} />
-            </LocationListStack.Navigator>
-      </NavigationContainer>
+      <UserProvider>
+        <NavigationContainer>
+              <LocationListStack.Navigator screenOptions={{ headerShown: false }}>
+                <LocationListStack.Screen name='Main' component={MainStackScreen} />
+              </LocationListStack.Navigator>
+        </NavigationContainer>
+      </UserProvider>
       <StatusBar style='auto' />
     </>
   )
