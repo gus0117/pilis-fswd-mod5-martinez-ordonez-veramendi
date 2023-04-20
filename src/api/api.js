47ed128739ebbc234fa68fba7a981ae031e7296a
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2023-04-19 22:33:29 -0300

    Agregada api de usuarios

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+const API_VERSION_NUMBER = 1
+
+export const api = {
+  server: 'https://643866984660f26eb19be700.mockapi.io', // completa con api server
+  apiVersion: `/api/v${API_VERSION_NUMBER}`
+}
