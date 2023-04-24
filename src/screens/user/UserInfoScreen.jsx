26710a2828fe819dfe2e5e282a702ca522c94ee1
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2023-04-23 22:30:29 -0300

    Agregado UserInfo con imagen de perfil por defecto

--- src/screens/user/UserInfoScreen.jsx
+++ src/screens/user/UserInfoScreen.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useContext } from 'react'
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native'
+import { styles } from './UserInfoScreen.styles'
+import { UserContext } from '../../contexts/UserContext'
 
 export const UserInfoScreen = () => {
+    const { currentUser, setCurrentUser } = useContext(UserContext)
+
+    const handleLogout = () => {
+        setCurrentUser(null)
+    }
+
     return(
-        <View><Text>UserInfoScreen</Text></View>
+        <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={styles.profileImage}
+          source={require('../../../assets/images/default-profile.png')}
+        />
+        <View style={styles.profileInfo}>
+          <Text style={styles.profileName}>{currentUser.username}</Text>
+          <Text style={styles.profileLocation}>Jujuy, Argentina</Text>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={handleLogout}>
+          <Text style={styles.buttonText}>Salir</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.sectionTitle}>About Me</Text>
+        <Text style={styles.sectionText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
+          tellus eu nisi tincidunt ultrices. Morbi id dictum ipsum. Nunc nec
+          lacus massa. Integer eget elit non elit sodales maximus.
+        </Text>
+        <Text style={styles.sectionTitle}>Interests</Text>
+        <Text style={styles.sectionText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
+          tellus eu nisi tincidunt ultrices. Morbi id dictum ipsum. Nunc nec
+          lacus massa. Integer eget elit non elit sodales maximus.
+        </Text>
+      </View>
+
+    </ScrollView>
     )
 }
